e3a6514e9467412e19ce329eb8d40dd55138a7b2
Mitesh <mitesh10001.js@gmail.com>
2024-02-13 11:38:32 +0000

    created and instanced a settings file to modify the game's apperance and behaviour

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,18 +1,19 @@
 import sys
 import pygame
+from settings import Settings
 
 class AlienInvasion():
 
     def __init__(self):
         pygame.init()
-
+        self.settings = Settings()
         self.clock = pygame.time.Clock()
         # game runs at a certain frame time 
 
-        self.screen = pygame.display.set_mode((1200,800))
+       
         pygame.display.set_caption("Alien Invasion")
-
-        self.bg_color = (230, 230, 230)
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+      
     
     def run_game(self):
         # start main loop of game
@@ -21,7 +22,7 @@ class AlienInvasion():
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
 
             #fills screen during each pass of loop
 
