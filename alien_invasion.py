a97e2cece6a4545b93a3ecd2515f02bc78ad52e2
Mitesh <mitesh10001.js@gmail.com>
2024-02-13 11:03:45 +0000

    setup the background colour and the frame rate of game

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,8 +6,13 @@ class AlienInvasion():
     def __init__(self):
         pygame.init()
 
+        self.clock = pygame.time.Clock()
+        # game runs at a certain frame time 
+
         self.screen = pygame.display.set_mode((1200,800))
         pygame.display.set_caption("Alien Invasion")
+
+        self.bg_color = (230, 230, 230)
     
     def run_game(self):
         # start main loop of game
@@ -16,9 +21,15 @@ class AlienInvasion():
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            self.screen.fill(self.bg_color)
+
+            #fills screen during each pass of loop
 
             # make most recently drawn screen visibile 
             pygame.display.flip()
+            self.clock.tick(60)
+            # runs at 60 frames
+
 
 if __name__ == '__main__':
         # make a game instance and run the game
