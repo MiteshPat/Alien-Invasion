1b361858e4426c41c9b0b47628c18f4e733976b9
Mitesh <mitesh10001.js@gmail.com>
2024-02-13 14:12:43 +0000

    Limited the ship's range by checking that it won't go past the borders

--- ship.py
+++ ship.py
@@ -23,9 +23,10 @@ class Ship:
 
     def update(self):
         #update ship based on movement flag
-        if self.moving_right:
+        #update ship's x value not the rect
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
         
         #update rect object from self.x.
