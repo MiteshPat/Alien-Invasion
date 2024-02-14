3d48d19f11dd2524a23ba60f6db3640f4b2c8250
Mitesh <mitesh10001.js@gmail.com>
2024-02-14 10:02:57 +0000

    Made a helper method for bullet changes, and restricted number of bullets to 3

--- alien_invasion.py
+++ alien_invasion.py
@@ -25,15 +25,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
-
-            # get rid of old bullets that have gone off screen
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
-            print(len(self.bullets))
-
-
+            self._update_bullets()
             self._update_screen()
     
             self.clock.tick(60)
@@ -71,8 +63,17 @@ class AlienInvasion:
     
     def _fire_bullet(self):
         # create bullet and add it to bullet group
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        # update position of bullets and get rid of old bullets
+        self.bullets.update()
+
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
     def _update_screen(self):
          # update images on screen, flip to new screen
