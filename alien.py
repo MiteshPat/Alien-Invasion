149c3fd1f27d583e09fc9718e730d69c8c6fda5e
Mitesh <mitesh10001.js@gmail.com>
2024-02-14 13:32:09 +0000

    Grid of Aliens can now respond to edges of screen and watch for any that reach the ship or hit the bottom

--- alien.py
+++ alien.py
@@ -20,7 +20,12 @@ class Alien(Sprite):
         # store alien's exact horizontal position
         self.x = float(self.rect.x)
 
+    def check_edges(self):
+        # return true if alien is at edge of screen
+        screen_rect = self.screen.get_rect()
+        return (self.rect.right >= screen_rect.right) or (self.rect.left <= 0)
+
     def update(self):
-        # move alien to the right
-        self.x += self.settings.alien_speed
+        # move alien to the right or left
+        self.x += self.settings.alien_speed * self.settings.fleet_direction
         self.rect.x = self.x
--- alien_invasion.py
+++ alien_invasion.py
@@ -104,9 +104,23 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
-    def _update_aliens(self):
-        # update positons of all aliens in the fleet.
+    def _check_fleet_edges(self):
+        # respond if alien reaches edge
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
 
+    def _change_fleet_direction(self):
+        # drop entire fleet and change fleet direction
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
+
+    def _update_aliens(self):
+        # check if fleet is at edge, then update positons.
+        self._check_fleet_edges()
         self.aliens.update()
 
 
